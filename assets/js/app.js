0569c6860feb0cabd55493b5c7ed7c57f17a9c16
Georgi Yanev <georgiyanev.gy@gmail.com>
2016-05-14 15:42:13 +0300

    add dynamic randomly rotating skill tags

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,35 @@
 $(document).ready(function() {
 
   //Init Skill tags
-  var skillTags = ["JavaScript", "HTML5", "CSS3", "jQuery"];
+  var skillTags = ["JavaScript", "HTML5", "CSS3", "jQuery", "Waypoints"];
 
   //Get all skill tags
   var getSkillTags = function() {
-    var allTags = '';
     //Gather all tags
     for (var tag in skillTags) {
-      allTags += skillTags[tag] + ' ';
+      var rotate = 0;
+      var zindex = 0;
+      var transformString = '';
+
+      var element = document.createElement("a");
+      var elText = document.createTextNode(skillTags[tag]);
+      element.appendChild(elText);
+      element.setAttribute("class", "skillTag");
+      element.setAttribute("href", "#CV");
+
+      //Randomly rotate left or right
+      Math.random() > 0.5 ? rotate = Math.random() * 10 : rotate = Math.random() * (-10);
+      zindex = Math.floor(Math.random() * 100);
+      transformString = "transform:rotateZ(" + rotate + "deg);-webkit-transform:rotateZ(" + rotate + "deg);-ms-transform:rotateZ(" + rotate + "deg);z-index:" + zindex;
+      element.setAttribute("style", transformString);
+
+      //Append the element
+      document.getElementById('outputSkillTags').appendChild(element);
     }
 
-    return allTags;
   };
 
-  //Update Skill tags UI
-  $('#outputSkillTags').html(getSkillTags);
+  //Update UI skill tags
+  getSkillTags();
 
 });
