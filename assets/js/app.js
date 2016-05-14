9dc4752b07662d7076491cdda1758949fb52f8e6
Georgi Yanev <georgiyanev.gy@gmail.com>
2016-05-14 16:27:34 +0300

    add tether to the project for bootstrap4 popovers; create dynamic skill tags

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,69 @@
 $(document).ready(function() {
 
-  //Init Skill tags
-  var skillTags = ["JavaScript", "HTML5", "CSS3", "jQuery", "Waypoints"];
+  //All skill data: name, type, url, experience
+  var skillData = [{
+    name: "JavaScript",
+    type: "language",
+    url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+    experience: "5 years"
+  }, {
+    name: "jQuery",
+    type: "framework",
+    url: "https://jquery.com/",
+    experience: "3 years"
+  }, {
+    name: "HTML5",
+    type: "markup language",
+    url: "http://www.w3schools.com/html/html5_intro.asp",
+    experience: "5 years"
+  }, {
+    name: "CSS3",
+    type: "language",
+    url: "http://www.w3schools.com/css/default.asp",
+    experience: "5 year"
+  }, {
+    name: "Atom",
+    type: "IDE/Code Editor",
+    url: "https://atom.io/",
+    experience: "1 year"
+  }, {
+    name: "Sublime Text 3",
+    type: "IDE/Code Editor",
+    url: "https://www.sublimetext.com/3",
+    experience: "2 years"
+  }, {
+    name: "Waypoints",
+    type: "framework",
+    url: "http://imakewebthings.com/waypoints/",
+    experience: "5 months"
+  }, {
+    name: "Intro.js",
+    type: "framework",
+    url: "http://introjs.com/",
+    experience: "2 months"
+  }];
 
   //Get all skill tags
-  var getSkillTags = function() {
-    //Gather all tags
-    for (var tag in skillTags) {
+  var populateSkillTags = function() {
+    //Itirate the tags
+    for (var tag in skillData) {
       var rotate = 0;
       var zindex = 0;
       var transformString = '';
 
+      //Create tag nodes
       var element = document.createElement("a");
-      var elText = document.createTextNode(skillTags[tag]);
+      var elText = document.createTextNode(skillData[tag].name);
       element.appendChild(elText);
+      //Set up attributes
       element.setAttribute("class", "skillTag");
-      element.setAttribute("href", "#CV");
+      element.setAttribute("tabindex", "0");
+      //Add pop overs
+      element.dataset.toggle = "popover";
+      element.dataset.trigger = "focus";
+      element.dataset.placement = "bottom";
+      element.dataset.title = skillData[tag].type + " <a target='_blank' href='" + skillData[tag].url + "'><i class='fa fa-link'></i></a>";
+      element.dataset.content = "XP: " + skillData[tag].experience;
 
       //Randomly rotate left or right
       Math.random() > 0.5 ? rotate = Math.random() * 10 : rotate = Math.random() * (-10);
@@ -30,6 +78,18 @@ $(document).ready(function() {
   };
 
   //Update UI skill tags
-  getSkillTags();
+  populateSkillTags();
+
+  //Init popovers
+  $('[data-toggle="popover"]').popover({
+    html: true
+  });
+  //Make popovers dismissable so less annoying
+  $('.popover-dismiss').popover({
+    trigger: 'focus'
+  });
+
+  //TODO
+  //Make the bottom section also get populated from the main skill data object
 
 });
