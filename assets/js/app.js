99b6b7c4179c7d78f768a5b4ee13f56c1b0d9be6
Georgi Yanev <georgiyanev.gy@gmail.com>
2016-05-15 18:50:48 +0300

    make resume section dynamic; introduce skill tag filters

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,22 +3,32 @@ $(document).ready(function() {
   //All skill data: name, type, url, experience
   var skillData = [{
     name: "JavaScript",
-    type: "language",
+    type: "Language",
     url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
     experience: "5 years"
   }, {
     name: "jQuery",
-    type: "framework",
+    type: "Framework",
     url: "https://jquery.com/",
     experience: "3 years"
   }, {
     name: "HTML5",
-    type: "markup language",
+    type: "Markup language",
     url: "http://www.w3schools.com/html/html5_intro.asp",
     experience: "5 years"
   }, {
+    name: "XML",
+    type: "Markup language",
+    url: "http://www.w3schools.com/xml/",
+    experience: "3 years"
+  }, {
+    name: "Markdown",
+    type: "Markup language",
+    url: "https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet",
+    experience: "1 year"
+  }, {
     name: "CSS3",
-    type: "language",
+    type: "Language",
     url: "http://www.w3schools.com/css/default.asp",
     experience: "5 year"
   }, {
@@ -27,48 +37,63 @@ $(document).ready(function() {
     url: "https://atom.io/",
     experience: "1 year"
   }, {
+    name: "WebStorm",
+    type: "IDE/Code Editor",
+    url: "https://www.jetbrains.com/webstorm/",
+    experience: "1 year"
+  }, {
+    name: "PyCharm",
+    type: "IDE/Code Editor",
+    url: "https://www.jetbrains.com/pycharm/",
+    experience: "1 year"
+  }, {
+    name: "Android Studio",
+    type: "IDE/Code Editor",
+    url: "http://developer.android.com/tools/studio/index.html",
+    experience: "1 year"
+  }, {
     name: "Sublime Text 3",
     type: "IDE/Code Editor",
     url: "https://www.sublimetext.com/3",
     experience: "2 years"
   }, {
     name: "Waypoints",
-    type: "framework",
+    type: "Framework",
     url: "http://imakewebthings.com/waypoints/",
     experience: "5 months"
   }, {
     name: "Intro.js",
-    type: "framework",
+    type: "Framework",
     url: "http://introjs.com/",
     experience: "2 months"
   }, {
     name: "Handlebars.js",
-    type: "framework",
+    type: "Framework",
     url: "http://handlebarsjs.com/",
     experience: "6 months"
   }, {
     name: "Moments.js",
-    type: "framework",
+    type: "Framework",
     url: "http://momentjs.com/",
     experience: "3 months"
   }, {
     name: "SQL",
-    type: "language",
+    type: "Language",
     url: "http://www.w3schools.com/sql/",
     experience: "3 years"
   }, {
     name: "PHP",
-    type: "language",
+    type: "Language",
     url: "http://php.net/",
     experience: "2 year"
   }, {
     name: "Bootstrap 3",
-    type: "framework",
+    type: "Framework",
     url: "http://getbootstrap.com/",
     experience: "2 years"
   }, {
     name: "Bootstrap 4",
-    type: "framework",
+    type: "Framework",
     url: "http://v4-alpha.getbootstrap.com/getting-started/introduction/",
     experience: "1 year"
   }, {
@@ -83,87 +108,87 @@ $(document).ready(function() {
     experience: "1 year"
   }, {
     name: "Git",
-    type: "tool",
+    type: "Tools",
     url: "https://git-scm.com/",
     experience: "3 years"
   }, {
     name: "Trello",
-    type: "tool",
+    type: "Tools",
     url: "https://www.trello.com/",
     experience: "1 year"
   }, {
     name: "JIRA",
-    type: "tool",
+    type: "Tools",
     url: "https://www.jira.com/",
     experience: "1 year"
   }, {
     name: "Agile",
-    type: "methodologies",
+    type: "Methodologies",
     url: "http://agilemethodology.org/",
     experience: "1 year"
   }, {
     name: "Node.js",
-    type: "library",
+    type: "Framework",
     url: "https://nodejs.org/en/",
     experience: "2 years"
   }, {
     name: "NPM",
-    type: "tool",
+    type: "Tools",
     url: "https://www.npmjs.com/",
     experience: "2 years"
   }, {
     name: "Gulp.js",
-    type: "tool",
+    type: "Tools",
     url: "http://gulpjs.com/",
     experience: "1 year"
   }, {
     name: "Grunt.js",
-    type: "tool",
+    type: "Tools",
     url: "http://gruntjs.com/",
     experience: "2 years"
   }, {
     name: "Firebase",
-    type: "framework",
+    type: "API",
     url: "https://www.firebase.com/",
     experience: "4 months"
   }, {
     name: "MongoDB",
-    type: "framework",
+    type: "Framework",
     url: "https://docs.mongodb.com",
     experience: "2 months"
   }, {
     name: "SQL",
-    type: "language",
+    type: "Language",
     url: "http://www.w3schools.com/sql/",
     experience: "2 years"
   }, {
     name: "Python 3",
-    type: "language",
+    type: "Language",
     url: "https://docs.python.org/3/",
     experience: "1 year"
   }, {
     name: "Material Design Lite",
-    type: "framework",
+    type: "Framework",
     url: "https://getmdl.io/",
     experience: "2 years"
   }, {
     name: "Hover.css",
-    type: "library",
+    type: "Library",
     url: "http://ianlunn.github.io/Hover/",
     experience: "1 year"
   }, {
     name: "Animate.css",
-    type: "library",
+    type: "Library",
     url: "https://daneden.github.io/animate.css/",
     experience: "1 year"
   }, {
     name: "Slack",
-    type: "tool",
+    type: "Tools",
     url: "https://slack.com",
     experience: "1 year"
   }, {
     name: "Assemble",
-    type: "framework",
+    type: "Framework",
     url: "http://assemble.io/",
     experience: "1 year"
   }, {
@@ -176,6 +201,21 @@ $(document).ready(function() {
     type: "CMS",
     url: "http://wordpress.org/",
     experience: "2 years"
+  }, {
+    name: "Java",
+    type: "Language",
+    url: "https://docs.oracle.com/javase/7/docs/api/",
+    experience: "3 years"
+  }, {
+    name: "Unity C#",
+    type: "Language",
+    url: "http://docs.unity3d.com/540/Documentation/Manual/",
+    experience: "3 years"
+  }, {
+    name: "Blender",
+    type: "3D modeling",
+    url: "https://www.blender.org/",
+    experience: "3 years"
   }];
 
   //Get all skill tags
@@ -194,6 +234,7 @@ $(document).ready(function() {
       element.setAttribute("class", "skillTag");
       element.setAttribute("tabindex", "0");
       //Add pop overs
+      element.dataset.type = skillData[tag].type;
       element.dataset.toggle = "popover";
       element.dataset.trigger = "focus";
       element.dataset.placement = "bottom";
@@ -206,17 +247,50 @@ $(document).ready(function() {
       transformString = "transform:rotateZ(" + rotate + "deg);-webkit-transform:rotateZ(" + rotate + "deg);-ms-transform:rotateZ(" + rotate + "deg);z-index:" + zindex;
       element.setAttribute("style", transformString);
 
-      //Append the element
+      //Append the element to skill pool
       document.getElementById('outputSkillTags').appendChild(element);
     }
+  };
 
+  //Collect and populate the resume section with the correct skills
+  var populateResumeSKills = function() {
+    var itemTypes = [];
+    var constructor = '';
+
+    //Itirate all skills and construct all types
+    for (var item in skillData) {
+      if (itemTypes.indexOf(skillData[item].type) < 0) {
+        itemTypes.push(skillData[item].type);
+      }
+    }
+
+    //Itirate all types and for each type all items in skills
+    for (var i in itemTypes) {
+      //Append the type heading
+      constructor += '<h6 class="text-primary">' + itemTypes[i] + '</h6><p>';
+      var firstItem = true;
+      for (var j in skillData) {
+        //Append all the items that match that type to that sub section
+        if (itemTypes[i] == skillData[j].type) {
+          //Add commas before all elements except first
+          if (!firstItem) {
+            constructor += ', ';
+          }
+          constructor += skillData[j].name;
+          firstItem = false;
+        }
+      }
+      //Close the paragraph
+      constructor += '</p>';
+    }
+
+    //Populate resume skills section
+    $('#outputResumeSkills').html(constructor);
   };
 
   //Update UI skill tags
   populateSkillTags();
-
-  //TODO
-  //Think of a way to either sort tags alphabetically or something else
+  populateResumeSKills();
 
   //Init popovers
   $('[data-toggle="popover"]').popover({
@@ -227,11 +301,23 @@ $(document).ready(function() {
     trigger: 'focus'
   });
 
-  //TODO
-  //Make the bottom section also get populated from the main skill data object
+  //Filter skill tags by type
+  $('.btn-tag-filter').click(function() {
+    //Get current filter
+    var filter = $(this).data("filter-value");
+    //If same button is pressed twice, restore tags to original state
+    if ($('.filter-elevated').data('type') != filter) {
+      $('.skillTag').removeClass("filter-elevated filter-out");
+    }
+    //Toggle correct filtered tags state
+    $('.skillTag[data-type="' + filter + '"]').toggleClass("filter-elevated");
+    $('.skillTag').not('[data-type="' + filter + '"]').toggleClass("filter-out");
+  });
 
-
-  //Easter egg
+  //
+  //SECRETS
+  //
+  //Logo easter egg
   $('.logo').click(function() {
     var clickCount = localStorage.getItem("GY_egg") || 0;
 
