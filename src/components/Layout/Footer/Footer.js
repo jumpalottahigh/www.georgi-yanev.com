454afb61ce331274d1ed6dea610e8fbc47ac622b
Georgi Yanev <georgiyanev.gy@gmail.com>
2018-12-29 19:36:29 +0200

    Add social links to footer and hide in mobile from header

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
-
+import github from '../Header/github.svg'
+import twitter from '../Header/twitter.svg'
+import './Footer.css'
 export default class Footer extends Component {
   render() {
     return (
       <footer>
+        <nav className="social">
+          <a href="https://github.com/jumpalottahigh">
+            <img src={github} alt="GitHub logo" />
+          </a>
+          <a href="https://twitter.com/jumpalottahigh">
+            <img src={twitter} alt="Twitter logo" />
+          </a>
+        </nav>
         <h5>
           Copyright &copy; 2013-2018 Georgi Yanev.
-          <br />Dedicated to Sofi and little Anton{' '}
+          <br />
+          Dedicated to Sofi and little Anton{' '}
           <span role="img" aria-label="heart">
             ❤️
           </span>
