64a84aea39b99940cd664970ec99b8b98b4dfe53
Georgi Yanev <georgiyanev.gy@gmail.com>
2019-01-08 23:46:39 +0200

    Create netlify redirects

--- src/components/Home/Inspirational/Inspirational.js
+++ src/components/Home/Inspirational/Inspirational.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Button from '../../elements/Button/Button'
 
 export default class Inspirational extends Component {
   render() {
@@ -7,13 +8,20 @@ export default class Inspirational extends Component {
         <p className="inspirational">
           <em>
             One of the reasons I keep my teenage hip hop pseudonim to this day
-            even in places like twitter and github, is because `<strong>
-              jumpalottahigh
-            </strong>` reflects my entire life - before, now and in the future -
-            I've always been trying to jump ever so slightly higher, to grow, to
-            evolve, to reach that next level!
+            even in places like twitter and github, is because `
+            <strong>jumpalottahigh</strong>` reflects my entire life - before,
+            now and in the future - I've always been trying to jump ever so
+            slightly higher, to grow, to evolve, to reach that next level!
           </em>
         </p>
+        <a
+          href="CV-Georgi_Yanev-2018.pdf"
+          target="_blank"
+          rel="noreferrer"
+          style={{ gridColumn: '1/-1', textAlign: 'center' }}
+        >
+          <Button>Resume</Button>
+        </a>
       </section>
     )
   }
