e78b5e6c21422264c64c5dd79c71dd0533203b92
Georgi Yanev <georgiyanev.gy@gmail.com>
2019-01-14 23:22:49 +0200

    Add a few short link redirects

--- src/components/Links/index.js
+++ src/components/Links/index.js
@@ -1,24 +1,52 @@
 import React from 'react'
+import styled from 'styled-components'
+import H2 from '../elements/H2/H2'
 
 const linkList = [
-  'https://gyanev.com/cv',
-  'https://gyanev.com/resume',
-  'https://gyanev.com/uses',
-  'https://gyanev.com/about',
-  'https://gyanev.com/blog',
+  { url: 'https://gyanev.com/cv', description: 'CV' },
+  { url: 'https://gyanev.com/resume', description: 'Resume' },
+  {
+    url: 'https://gyanev.com/uses',
+    description: 'Code editor, terminal, plugins, settings, themes',
+  },
+  {
+    url: 'https://gyanev.com/about',
+    description:
+      'Projects, Skills, Certificates, Hobbies all in one looong page',
+  },
+  { url: 'https://gyanev.com/blog', description: 'My blog' },
+  { url: 'https://gyanev.com/dot', description: 'Dotfiles' },
+  { url: 'https://gyanev.com/npm', description: 'My first NPM package' },
+  { url: 'https://gyanev.com/github', description: 'GitHub profile' },
+  { url: 'https://gyanev.com/twitter', description: 'Twitter profile' },
+  { url: 'https://gyanev.com/linkedin', description: 'LinkedIn profile' },
+  { url: 'https://gyanev.com/twitch', description: 'Twitch profile' },
 ]
 
+const LinkContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+
+  .inner {
+    max-width: 80ch;
+    margin: 0 auto;
+  }
+`
+
 const Links = () => (
-  <div>
-    Links:
-    {linkList.map((link, index) => (
-      <div key={index}>
-        <a href={link} target="_blank" rel="noreferrer">
-          {link}
-        </a>
-      </div>
-    ))}
-  </div>
+  <LinkContainer>
+    <H2>Short Links:</H2>
+    <div className="inner">
+      {linkList.map((link, index) => (
+        <div key={index}>
+          <a href={link.url} target="_blank" rel="noopener noreferrer">
+            {link.url}
+          </a>
+          <p>{link.description}</p>
+        </div>
+      ))}
+    </div>
+  </LinkContainer>
 )
 
 export default Links
